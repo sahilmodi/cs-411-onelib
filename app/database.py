aef7e27e0b297c70bed7c7c8872c9256c51ca6c4
smodi9 <smodi9@illinois.edu>
2021-10-21 17:30:22 -0500

    added borrowed books insert

--- app/database.py
+++ app/database.py
@@ -1,10 +1,42 @@
 from app import db
+import datetime
+
+tables = {"User", "Review", "BorrowedBook", "Library", "LibraryBook", "Book"}
 
 def test():
+    return read_from_table("Book", 5)
+
+def _search(table, field, keyword, amount=5):
+    assert table in tables
     with db.begin() as conn:
-        res = conn.execute("Select * from User;").fetchmany(5);
-        print(res)
+        res = conn.execute(f"SELECT * FROM {table} WHERE {field} LIKE '%{keyword}%").fetchmany(amount)
     return res
 
-def search(keyword, table):
-    pass
+def search_review(keyword, amount):
+    return _search("Review", "Text", keyword, amount)
+
+def search_library(keyword, amount):
+    return _search("Library", "Name", keyword, amount)
+
+def add_borrowed_book(user_id, library_id, isbn):
+    conn = db.connect()
+    conn.execute("TRUNCATE BorrowedBook")
+    res = conn.execute(f"SELECT Quantity, TimeLimitDays FROM LibraryBook WHERE LibraryID={library_id} AND ISBN LIKE '{isbn}'").fetchone()
+    if res is None:
+        return False
+    quantity, time_limit_days = res
+    if quantity == 0:
+        return False
+    quantity -= 1
+    due_date = datetime.datetime.today().date() + datetime.timedelta(days=time_limit_days)
+    q = f"Insert INTO BorrowedBook VALUES ({user_id}, {library_id}, '{isbn}', '{due_date}')"
+    print(q)
+    conn.execute(q)
+    conn.close()
+    return True
+
+def read_from_table(table, amount=5):
+    assert table in tables
+    with db.begin() as conn:
+        res = conn.execute(f"SELECT * FROM {table}").fetchmany(amount)
+    return res
--- app/routes.py
+++ app/routes.py
@@ -7,3 +7,9 @@ from flask import jsonify
 def homepage():
     users = db_helper.test()
     return jsonify({r[0]:str(r[1:]) for r in users})
+
+@app.route("/add")
+def add():
+    db_helper.add_borrowed_book(0, 0, "019509199X")
+    bb = db_helper.read_from_table("BorrowedBook")
+    return jsonify({r[0]:str(r[1:]) for r in bb})
