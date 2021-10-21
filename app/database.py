6285d42581847246b0af558419031928fb0e9092
xxxuanl <91907153+xxxuanl@users.noreply.github.com>
2021-10-21 17:38:46 -0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -40,3 +40,90 @@ def read_from_table(table, amount=5):
     with db.begin() as conn:
         res = conn.execute(f"SELECT * FROM {table}").fetchmany(amount)
     return res
+
+
+def fetch_TopBook() -> dict:
+    """
+    Return the Top 10 books that have the most number of 5 stars reviews in recent 2 years.
+
+    Returns:
+        A list of dictionaries
+    """
+
+    conn = db.connect()
+    query='(SELECT Title, YEAR(Date) AS Year \
+            FROM Review NATURAL JOIN Book \
+            WHERE StarRating=5 AND YEAR(Date)=2021 \
+            GROUP BY ISBN, YEAR(Date) \
+            ORDER BY COUNT( * ) DESC LIMIT 10) \
+            UNION ALL \
+            (SELECT Title, YEAR(Date) AS Year \
+            FROM Review NATURAL JOIN Book \
+            WHERE StarRating=5 AND YEAR(Date)=2020 \
+            GROUP BY ISBN, YEAR(Date) \
+            ORDER BY COUNT( * ) DESC LIMIT 10);'
+    query_results = conn.execute(query).fetchall()
+    conn.close()
+    TopBook = []
+    for result in query_results:
+        item = {
+            "Title": result[0],
+            "Year": result[1]
+        }
+        TopBook.append(item)
+    return TopBook
+
+def fetch_TopUser() -> dict:
+    """
+    Return the Top 10 Users who have the most number of reviews in recent 2 years.
+
+    Returns:
+        A list of dictionaries
+    """
+
+    conn = db.connect()
+    query='(SELECT Name, YEAR(Date)\
+            FROM Review NATURAL JOIN User \
+            WHERE YEAR(Date)=2021 \
+            GROUP BY Review.UserID, YEAR(Date) \
+            ORDER BY COUNT(*) DESC LIMIT 10) \
+            UNION ALL \
+            (SELECT Name, YEAR(Date) \
+            FROM Review NATURAL JOIN User \
+            WHERE YEAR(Date)=2020 \
+            GROUP BY Review.UserID, YEAR(Date) \
+            ORDER BY COUNT(*) DESC LIMIT 10);'
+    query_results = conn.execute(query).fetchall()
+    conn.close()
+    TopUser = []
+    for result in query_results:
+        item = {
+            "Name": result[0],
+            "Year": result[1]
+        }
+        TopUser.append(item)
+    return TopUser
+
+def update_rent_librarybook(LibraryID: int, ISBN: str) -> None:
+    """
+    Update the Quantity after user rent a book
+
+    """
+    conn = db.connect()
+    Quantity = conn.execute('Select Quantity from LibraryBook where LibraryID = {} and ISBN="{}";'.format(LibraryID,ISBN))
+    BookQuantity=int(Quantity)
+    query = 'Update LibraryBook set Quantity = {} where LibraryID = {} and ISBN="{}";'.format(BookQuantity-1, LibraryID, ISBN)
+    conn.execute(query)
+    conn.close()
+
+def update_return_librarybook(LibraryID: int, ISBN: str) -> None:
+    """
+    Update the Quantity after user return a book
+
+    """
+    conn = db.connect()
+    Quantity = conn.execute('Select Quantity from LibraryBook where LibraryID = {} and ISBN="{}";'.format(LibraryID,ISBN))
+    BookQuantity=int(Quantity)
+    query = 'Update LibraryBook set Quantity = {} where LibraryID = {} and ISBN="{}";'.format(BookQuantity+1, LibraryID, ISBN)
+    conn.execute(query)
+    conn.close()
